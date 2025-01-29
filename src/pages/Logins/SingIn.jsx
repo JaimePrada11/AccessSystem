c8975d519fc40fb27de29e8ef88672e4f46f2f15
JaimePrada11 <jandrespradato.11@gmail.com>
2025-01-28 23:55:45 -0500

    feat: :sparkles: Sign in form

--- src/pages/Logins/SingIn.jsx
+++ src/pages/Logins/SingIn.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import { AiOutlineUser, AiOutlineIdcard, AiOutlinePhone } from 'react-icons/ai';
 import { FaRegCalendarAlt } from 'react-icons/fa';
+import { BiLock, BiUser } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
+import axiosInstance from '../../Services/apiService';
+import { useNavigate } from "react-router";
 
 const SignIn = () => {
+  const [step, setStep] = useState(1);
   const [formData, setFormData] = useState({
     name: "",
     cedula: "",
     phone: "",
-    employmentDate: ""
+    employmentDate: "",
+    username: "",
+    password: ""
   });
   const [errors, setErrors] = useState({});
+  const [cedulaExists, setCedulaExists] = useState(false);
+  const [usernameExists, setUsernameExists] = useState(false);
+  const navigate = useNavigate();
+
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -20,57 +30,201 @@ const SignIn = () => {
     }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const validateStep1 = async () => {
     const formErrors = {};
-    Object.keys(formData).forEach(key => {
-      if (!formData[key]) formErrors[key] = `${key.charAt(0).toUpperCase() + key.slice(1)} cannot be empty`;
+    ["name", "cedula", "phone", "employmentDate"].forEach(field => {
+      if (!formData[field]) formErrors[field] = `${field.charAt(0).toUpperCase() + field.slice(1)} cannot be empty`;
     });
     setErrors(formErrors);
 
-    if (Object.keys(formErrors).length === 0) {
-      console.log("Form submitted successfully!");
+    try {
+      const cedulaResponse = await axiosInstance.get(`/porters`);
+      const cedulaInUse = cedulaResponse.data.some(porter => porter.cedula === formData.cedula); // Busca la cédula en la lista de porteros
+
+      if (cedulaInUse) {
+        setCedulaExists(true);
+      } else {
+        setCedulaExists(false);
+      }
+    } catch (error) {
+      console.error("Error verifying cedula:", error);
+      setCedulaExists(false);
+    }
+
+    return Object.keys(formErrors).length === 0 && !cedulaExists;
+  };
+
+  const validateStep2 = async () => {
+    const formErrors = {};
+    if (!formData.username) formErrors.username = "Username cannot be empty";
+    if (!formData.password) formErrors.password = "Password cannot be empty";
+
+    try {
+      const usernameResponse = await axiosInstance.get(`/user`);
+      const usernameInUse = usernameResponse.data.some(user => user.userName === formData.username);
+
+      if (usernameInUse) {
+        setUsernameExists(true);
+      } else {
+        setUsernameExists(false);
+      }
+    } catch (error) {
+      console.error("Error verifying username:", error);
+      setUsernameExists(false);
+    }
+
+    setErrors(formErrors);
+    return Object.keys(formErrors).length === 0 && !usernameExists;
+  };
+
+  const handleNext = async () => {
+    if (await validateStep1()) setStep(2);
+  };
+
+  const handleBack = () => {
+    setStep(1);
+    setErrors({});
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!(await validateStep2())) return;
+
+    const transformedData = {
+      name: formData.name,
+      cedula: formData.cedula,
+      telefono: formData.phone,
+      employmentDate: formData.employmentDate
+    };
+
+    const userData = {
+      userName: formData.username,
+      password: formData.password
+    };
+
+    try {
+      const porterResponse = await axiosInstance.post(`/porters`, transformedData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const porterId = porterResponse.data.id;
+
+      if (!porterId) {
+        console.error("Porter ID is not returned.");
+        return;
+      }
+
+      const responseUser = await axiosInstance.post(`user/porter/${porterId}`, userData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      alert('User created successfully')
+      navigate("/login");
+
+    } catch (error) {
+      console.error("Error during form submission:", error);
     }
   };
 
+
   return (
-    <div className="flex items-center  justify-center min-h-screen bg-gradient-to-br from-black to-blue-900">
-      <section className="bg-white/10 mt-6 mb-5 backdrop-blur-md bg-opacity-20 p-8 rounded-lg shadow-lg w-full max-w-md">
-        <form className="space-y-6 " onSubmit={handleSubmit}>
-          <h2 className="text-3xl text-white font-bold text-center text-gray-800 uppercase">Sign Up</h2>
-          <div className="space-y-5">
-            {["name", "cedula", "phone", "employmentDate"].map((field, index) => {
-              const labelText = field.charAt(0).toUpperCase() + field.slice(1);
-              const Icon = field === "name" ? AiOutlineUser : field === "cedula" ? AiOutlineIdcard : field === "phone" ? AiOutlinePhone : FaRegCalendarAlt;
-              const type = field === "employmentDate" ? "date" : "text";
-              
-              return (
-                <div key={index} className="relative flex flex-col">
-                  <label htmlFor={field} className="mb-2 font-semibold text-gray-200">{labelText}</label>
-                  <Icon className="absolute left-3 top-10 text-white" size={24} />
-                  <input
-                    type={type}
-                    name={field}
-                    id={field}
-                    placeholder={`Enter your ${labelText.toLowerCase()}`}
-                    className="p-3 pl-12 text-white border-b border-gray-700 bg-transparent placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                    value={formData[field]}
-                    onChange={handleChange}
-                  />
-                  {errors[field] && <span className="text-red-500 text-sm mt-1">{errors[field]}</span>}
-                </div>
-              );
-            })}
-          </div>
-          <button 
-            type="submit" 
-            className="w-full px-4 py-3 bg-blue-900 text-white font-medium rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer"
-          >
-            Sign Up
-          </button>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-black to-blue-900">
+      <section className="m-5 bg-white/10 backdrop-blur-md bg-opacity-20 p-8 rounded-lg shadow-lg w-full max-w-md">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <h2 className="text-3xl text-white font-bold text-center uppercase">Sign Up</h2>
+
+          {step === 1 ? (
+            <>
+              {/* Datos Personales */}
+              <div className="space-y-5">
+                {["name", "cedula", "phone", "employmentDate"].map((field, index) => {
+                  const labelText = field.charAt(0).toUpperCase() + field.slice(1);
+                  const Icon = field === "name" ? AiOutlineUser : field === "cedula" ? AiOutlineIdcard : field === "phone" ? AiOutlinePhone : FaRegCalendarAlt;
+                  const type = field === "employmentDate" ? "date" : "text";
+
+                  return (
+                    <div key={index} className="relative flex flex-col">
+                      <label htmlFor={field} className="mb-2 font-semibold text-gray-200">{labelText}</label>
+                      <Icon className="absolute left-3 top-10 text-white" size={24} />
+                      <input
+                        type={type}
+                        name={field}
+                        id={field}
+                        placeholder={`Enter your ${labelText.toLowerCase()}`}
+                        className="p-3 pl-12 text-white border-b border-gray-700 bg-transparent placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        value={formData[field]}
+                        onChange={handleChange}
+                      />
+                      {errors[field] && <span className="text-red-500 text-sm mt-1">{errors[field]}</span>}
+                      {cedulaExists && field === "cedula" && <span className="text-red-500 text-sm mt-1">Cédula ya está en uso</span>}
+                    </div>
+                  );
+                })}
+              </div>
+
+              <button
+                type="button"
+                onClick={handleNext}
+                className="w-full px-4 py-3 bg-blue-900 text-white font-medium rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer"
+              >
+                Next
+              </button>
+            </>
+          ) : (
+            <>
+              {/* Credenciales */}
+              <div className="space-y-5">
+                {["username", "password"].map((field, index) => {
+                  const labelText = field.charAt(0).toUpperCase() + field.slice(1);
+                  const Icon = field === "username" ? BiUser : BiLock;
+                  const type = field === "password" ? "password" : "text";
+
+                  return (
+                    <div key={index} className="relative flex flex-col">
+                      <label htmlFor={field} className="mb-2 font-semibold text-gray-200">{labelText}</label>
+                      <Icon className="absolute left-3 top-10 text-white" size={24} />
+                      <input
+                        type={type}
+                        name={field}
+                        id={field}
+                        placeholder={`Enter your ${labelText.toLowerCase()}`}
+                        className="p-3 pl-12 text-white border-b border-gray-700 bg-transparent placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                        value={formData[field]}
+                        onChange={handleChange}
+                      />
+                      {errors[field] && <span className="text-red-500 text-sm mt-1">{errors[field]}</span>}
+                      {usernameExists && field === "username" && <span className="text-red-500 text-sm mt-1">Username is already taken</span>}
+                    </div>
+                  );
+                })}
+              </div>
+
+              <div className="flex justify-between">
+                <button
+                  type="button"
+                  onClick={handleBack}
+                  className="px-4 py-3 bg-gray-600 text-white font-medium rounded-lg hover:bg-gray-500 transition duration-300 cursor-pointer"
+                >
+                  Back
+                </button>
+                <button
+                  type="submit"
+                  className="px-4 py-3 bg-blue-900 text-white font-medium rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </>
+          )}
+
           <div className="flex flex-row items-center justify-center mt-4 text-gray-200">
             <p className="mr-2">Already have an account?</p>
-            <Link to="/login" className="text-blue-400 font-semibold hover:text-blue-700 transition duration-300 cursor-pointer">
+            <Link to="/" className="text-blue-400 font-semibold hover:text-blue-700 transition duration-300 cursor-pointer">
               Login
             </Link>
           </div>
--- src/pages/Logins/login.jsx
+++ src/pages/Logins/login.jsx
@@ -2,8 +2,13 @@ import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router";
 import UserContext from "../../Context";
 import { axiosInstanceLogin } from "../../Services/apiService";
+import useApi from "../../hooks/useData";
+import { CiUser } from "react-icons/ci";
+import { FaKey } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const { data: porters } = useApi("/porters");
     const { login } = useContext(UserContext);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -11,7 +16,7 @@ const Login = () => {
     const [message, setMessage] = useState("");
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
         let formErrors = {};
@@ -19,87 +24,119 @@ const Login = () => {
         if (!password) formErrors.password = "Password cannot be empty";
         setErrors(formErrors);
 
-        if (Object.keys(formErrors).length > 0) {
-            return;
+        if (Object.keys(formErrors).length > 0) return;
+
+        try {
+            const response = await axiosInstanceLogin.post(
+                "login",
+                new URLSearchParams({ userName: username, password })
+            );
+
+            if (!response.data || !response.data.token) {
+                setMessage("Invalid username or password");
+                return;
+            }
+
+            const { token, userName } = response.data;
+            localStorage.setItem("authToken", token);
+
+            if (porters && porters.length > 0) {
+                const userf = porters.find(
+                    (porter) => porter.user.userName === username
+                );
+
+                if (userf) {
+                    login(userf);
+                } else {
+                    login({ userName }); 
+                }
+            } else {
+                login({ userName });
+            }
+
+            alert("Welcome ! ");
+            navigate("/home");
+
+        } catch (error) {
+            if (error.response) {
+                console.error("Respuesta del servidor:", error.response.data);
+                setMessage("Invalid username or password");
+            } else {
+                console.error("Error de conexión:", error.message);
+                alert("Hubo un problema al intentar conectarse con el servidor.");
+            }
         }
+    };
 
-        
-    axiosInstanceLogin.post("login", new URLSearchParams({ userName: username, password }))
-    .then((response) => {
-        if (!response.data || !response.data.token) {
-            setMessage("Invalid username or password");
-            return;
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name === "username") {
+          setUsername(value);
+        } else if (name === "password") {
+          setPassword(value);
         }
-
-        const { token, userName } = response.data;
-
-        // Guardar token en localStorage
-        localStorage.setItem("authToken", token);
-        
-        // Guardar usuario en el contexto
-        login({ userName });
-
-        alert("Bienvenido/a!");
-        navigate("/home");
-    })
-    .catch((error) => {
-        if (error.response) {
-            console.error("Respuesta del servidor:", error.response.data);
-            setMessage("Invalid username or password");
-        } else {
-            console.error("Error de conexión:", error.message);
-            alert("Hubo un problema al intentar conectarse con el servidor.");
-        }
-    });
-
     };
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-900 to-red-100">
-            <section className="bg-white/10 backdrop-blur-md bg-opacity-20 p-8 rounded-lg shadow-lg w-full max-w-md">
+            <section className="m-5 bg-white/10 backdrop-blur-md bg-opacity-20 p-8 rounded-lg shadow-lg w-full max-w-md">
                 <form className="space-y-6" onSubmit={handleSubmit}>
                     <h2 className="text-3xl font-bold text-center text-gray-800 uppercase">Sign In</h2>
                     <div className="space-y-5">
                         <div className="relative flex flex-col">
                             <label htmlFor="username" className="mb-2 font-semibold text-gray-200">Username</label>
+                            <CiUser className="absolute text-white  left-3 top-10 text-gray-400" size={24} />
                             <input
                                 type="text"
                                 name="username"
                                 id="username"
                                 placeholder="Enter your username"
-                                className="p-3 text-white border-b border-gray-700 bg-transparent placeholder-white focus:outline-none focus:ring-0"
+                                autoComplete="off"
+                                className="p-3 pl-12 text-white border-b border-gray-700 bg-transparent placeholder-white focus:outline-none focus:ring-0"
                                 value={username}
-                                onChange={(e) => setUsername(e.target.value)}
+                                onChange={handleChange}
                             />
                             {errors.username && <span className="text-red-500 text-sm mt-1">{errors.username}</span>}
                         </div>
                         <div className="relative flex flex-col">
                             <label htmlFor="password" className="mb-2 font-semibold text-gray-200">Password</label>
+                            <FaKey className="absolute text-white left-3 top-10 text-gray-400" size={24} />
                             <input
                                 type="password"
                                 name="password"
                                 id="password"
                                 placeholder="Enter your password"
-                                className="p-3 text-white border-b border-gray-700 bg-transparent placeholder-white focus:outline-none focus:ring-0"
+                                className="p-3 pl-12 text-white border-b border-gray-700 bg-transparent  placeholder-white focus:outline-none focus:ring-0"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={handleChange}
                             />
                             {errors.password && <span className="text-red-500 text-sm mt-1">{errors.password}</span>}
                         </div>
                     </div>
-                    <button
-                        type="submit"
+                    <button 
+                        type="submit" 
                         className="w-full px-4 py-3 bg-blue-500 text-white font-medium rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer"
                     >
                         Enter
                     </button>
                     <div className="mt-4">
-                        {message && <span className={`text-sm ${message === "Invalid username or password" ? "text-red-500" : "text-green-500"}`}>{message}</span>}
+                        {message && <span className={`text-sm ${message === "Access granted" ? "text-green-500" : "text-red-500"}`}>{message}</span>}
+                    </div>
+                    <div className="flex flex-row items-center justify-center mt-4 text-gray-200">
+                        <p className="mr-2">Don't have an account?</p>
+                        <Link to="/signin">
+                        <button 
+                            type="button" 
+                            className="text-blue-400 font-semibold hover:text-blue-700 transition duration-300 cursor-pointer"
+                        >
+                            Sign Up
+                        </button>
+                        </Link>
+                        
                     </div>
                 </form>
             </section>
         </div>
     );
-};
-
+}
 export default Login;
